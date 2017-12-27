9dfd49c8384eabe39a7832caa68eace31e51d2ed
Ramesh <Ramesh.Babu@neustar.biz>
2017-12-27 16:26:44 +0530

    dec27

---
+++ Later/Spring/4_Dependency_Injection/5/SpringDemo/src/main/java/com/ram/core/App.java
@@ -0,0 +1,21 @@
+package com.ram.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		System.out.println("---------------------------------------");
+
+		Employee employee = (Employee) context.getBean("employee");
+		System.out.println("Got employee object from the ApplicationContext");
+		employee.displayEmployeeDetails();
+
+	}
+
+}
