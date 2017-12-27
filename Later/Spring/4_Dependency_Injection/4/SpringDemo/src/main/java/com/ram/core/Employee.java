c1c18eea05471c26ed2160228d4e836543d22595
Ramesh <Ramesh.Babu@neustar.biz>
2017-12-27 16:03:25 +0530

    dec27

---
+++ Later/Spring/4_Dependency_Injection/4/SpringDemo/src/main/java/com/ram/core/Employee.java
@@ -0,0 +1,35 @@
+package com.ram.core;
+
+public class Employee
+{
+	private int id;
+	private String name;
+
+	public Employee(int id)
+	{
+		System.out.println("Employee(int id) constructor is called "
+				+ "by the Spring container");
+		this.id = id;
+	}
+
+	public Employee(String name)
+	{
+		System.out.println("Employee(String name) constructor is called "
+				+ "by the Spring container");
+		this.name = name;
+	}
+
+	public Employee(int id, String name)
+	{
+		System.out.println("Employee(int id, String name) constructor "
+				+ "is called by the Spring container");
+		this.id = id;
+		this.name = name;
+	}
+
+	public void displayEmployeeDetails()
+	{
+		System.out.println("Employee [id=" + id + ", name=" + name + "]");
+	}
+
+}
