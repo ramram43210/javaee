08437dd2162a1e73c37511b739699ffd85f05260
Ramesh <Ramesh.Babu@neustar.biz>
2017-12-28 17:15:53 +0530

    dec28

---
+++ Later/Spring/4_Dependency_Injection/7/SpringDemo/src/main/java/com/ram/core/Address.java
@@ -0,0 +1,23 @@
+package com.ram.core;
+
+public class Address
+{
+	private String city;
+	private String state;
+	private String country;
+
+	public Address(String city, String state, String country)
+	{
+		this.city = city;
+		this.state = state;
+		this.country = country;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Address [city=" + city + ", state=" + state + ", country="
+				+ country + "]";
+	}
+
+}
---
+++ Later/Spring/4_Dependency_Injection/7/SpringDemo/src/main/java/com/ram/core/Employee.java
@@ -0,0 +1,24 @@
+package com.ram.core;
+
+public class Employee
+{
+	private int id;
+	private String name;
+	private Address address;
+
+	public Employee(int id, String name, Address address)
+	{
+		System.out.println("Employee(int id, String name, Address address) "
+				+ "constructor is called by the Spring container");
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+	public void displayEmployeeDetails()
+	{
+		System.out.println("Employee [id=" + id + ", name=" + name
+				+ ", address=" + address + "]");
+	}
+
+}
