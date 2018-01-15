b983004578f14bddeaac09f5e7707642273fe186
Ramesh <Ramesh.Babu@neustar.biz>
2018-01-15 18:39:38 +0530

    jan15

---
+++ Later/Spring/4_Dependency_Injection/19/SpringDemo/src/main/java/com/ram/core/App.java
@@ -0,0 +1,22 @@
+package com.ram.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		System.out.println("---------------------------------------");
+
+		FruitShop fruitShop = (FruitShop) context.getBean("fruitShop");
+		System.out.println(
+				"Got fruitShop object from the ApplicationContext(SpringContainer)");
+		fruitShop.displayFruitShopInfo();
+
+	}
+
+}
