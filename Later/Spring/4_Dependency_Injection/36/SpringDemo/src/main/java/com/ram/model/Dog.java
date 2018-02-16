146ed95877060c10d3bab00cf5628128a98dc5fc
Ram N <ramram_43210@yahoo.com>
2018-02-16 18:47:55 +0530

    feb16

---
+++ Later/Spring/4_Dependency_Injection/36/SpringDemo/src/main/java/com/ram/model/Dog.java
@@ -0,0 +1,14 @@
+package com.ram.model;
+
+/*
+ * This class is a singleton class
+ */
+public class Dog implements Animal
+{
+
+	public void eat()
+	{
+		System.out.println("Dog eats chicken bone...");
+	}
+
+}
