6e35c6e03c3b39fc12ad2badb51b47ab7577050a
Ram N <ramram_43210@yahoo.com>
2018-02-22 15:35:09 +0530

    feb22

---
+++ Later/Spring/4_Dependency_Injection/37/SpringDemo/src/main/java/com/ram/model/Animal.java
@@ -0,0 +1,6 @@
+package com.ram.model;
+
+public interface Animal
+{
+	public abstract void eat();
+}
---
+++ Later/Spring/4_Dependency_Injection/37/SpringDemo/src/main/java/com/ram/model/Cow.java
@@ -0,0 +1,11 @@
+package com.ram.model;
+
+public class Cow implements Animal
+{
+
+	public void eat()
+	{
+		System.out.println("Cow eats grass..");
+	}
+
+}
