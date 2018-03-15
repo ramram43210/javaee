b09d1c81cf7cf1651021f7c129c041f34d0acba1
Ram <ramram_43210@yahoo.com>
2018-03-15 14:40:10 +0530

    mar15

---
+++ Later/Spring/5_Spring_Basic/19/SpringDemo/src/main/java/com/ram/model/Employee.java
@@ -0,0 +1,49 @@
+package com.ram.model;
+
+import org.springframework.beans.factory.annotation.Required;
+
+public class Employee
+{
+	private int id;
+	private String name;
+	private Address address;
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public Address getAddress()
+	{
+		return address;
+	}
+	
+	@Required
+	public void setAddress(Address address)
+	{
+		this.address = address;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Employee [id=" + id + ", name=" + name + ", address=" + address
+				+ "]";
+	}
+
+}
