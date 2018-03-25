aa73fa6aad41fe2fc5517ca43f253eaac06ae358
Ram <ramram_43210@yahoo.com>
2018-03-25 08:57:49 +0530

    mar25

---
+++ Spring/SpringHelloWorld_import_eclipse/SpringHelloWorld/src/main/java/com/ram/core/App.java
@@ -0,0 +1,19 @@
+package com.ram.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"SpringBeans.xml");
+
+		HelloWorld helloWorldObj = (HelloWorld) context.getBean("helloBean");
+		System.out.println(
+				"Got helloWorldObj from the ApplicationContext(Spring Container)");
+		helloWorldObj.printHello();
+
+	}
+}
