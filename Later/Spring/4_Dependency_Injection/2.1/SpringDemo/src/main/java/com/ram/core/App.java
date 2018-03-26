0b434eedecd892798b4c66cdfa230aefb3e10b2d
Ram <ramram_43210@yahoo.com>
2018-03-26 18:26:42 +0530

    mar26

---
+++ Later/Spring/4_Dependency_Injection/2.1/SpringDemo/src/main/java/com/ram/core/App.java
@@ -0,0 +1,48 @@
+package com.ram.core;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		Resource resource = new ClassPathResource("applicationContext.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);
+
+		System.out.println("---------------------------------------");
+
+		// 1 st way of getting employee bean
+		Object object = factory.getBean("employee");
+		Employee employee = (Employee) object;
+		System.out.println("Got employee object from the BeanFactory");
+		employee.employeeDetails();
+
+		// 2nd way of getting employee bean
+		employee = factory.getBean("employee", Employee.class);
+		System.out.println("\nGot employee object from the BeanFactory");
+		employee.employeeDetails();
+
+		// 3rd way of getting employee bean
+		employee = factory.getBean(Employee.class);
+		System.out.println("\nGot employee object from the BeanFactory");
+		employee.employeeDetails();
+
+		boolean result = factory.containsBean("employee");
+		System.out.println("\nis employee bean available? = " + result);
+
+		boolean isSingleton = factory.isSingleton("employee");
+		System.out.println(
+				"\nis employee bean Singleton[always return the same instance]? = "
+						+ isSingleton);
+
+		boolean isPrototype = factory.isPrototype("employee");
+		System.out.println(
+				"\nis employee bean isPrototype[always return independent instances]? = "
+						+ isPrototype);
+
+	}
+
+}
