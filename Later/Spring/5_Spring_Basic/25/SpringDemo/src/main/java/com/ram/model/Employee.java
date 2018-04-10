516e6613fd8cd742052e0c24079b65c41d26bc4b
Ram <ramram_43210@yahoo.com>
2018-04-10 16:03:47 +0530

    apr10

---
+++ Later/Spring/5_Spring_Basic/25/SpringDemo/src/main/java/com/ram/model/Employee.java
@@ -0,0 +1,21 @@
+package com.ram.model;
+
+/**
+ * Spring bean
+ */
+public class Employee
+{
+	private String name;
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Employee [name=" + name + "]";
+	}
+
+}
