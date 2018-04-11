2cf431733d82de13dbc516f2d703d7285a938827
Ram <ramram_43210@yahoo.com>
2018-04-11 10:02:47 +0530

    apr11

---
+++ Spring_2018/SpringDemo_alias_name/SpringDemo/src/main/java/com/ram/model/Employee.java
@@ -0,0 +1,19 @@
+package com.ram.model;
+
+/**
+ * Spring bean
+ */
+public class Employee
+{
+	private String name;
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public void employeeDetails()
+	{
+		System.out.println("Employee Name =  " + name);
+	}
+}
