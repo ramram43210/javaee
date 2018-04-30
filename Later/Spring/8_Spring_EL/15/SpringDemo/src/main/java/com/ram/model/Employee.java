e47c886909c71a5b4144201387148046e20bb993
Ram <ramram_43210@yahoo.com>
2018-04-30 17:50:37 +0530

    apr30

---
+++ Later/Spring/8_Spring_EL/15/SpringDemo/src/main/java/com/ram/model/Employee.java
@@ -0,0 +1,69 @@
+package com.ram.model;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("employee")
+public class Employee
+{
+
+	@Value("10")
+	private int id;
+
+	@Value("Peter")
+	private String name;
+
+	@Value("#{addressInfo.addressList[0]}")
+	private String firstAddress;
+
+	@Value("#{addressInfo.addressList[1]}")
+	private String secondAddress;
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getFirstAddress()
+	{
+		return firstAddress;
+	}
+
+	public void setFirstAddress(String firstAddress)
+	{
+		this.firstAddress = firstAddress;
+	}
+
+	public String getSecondAddress()
+	{
+		return secondAddress;
+	}
+
+	public void setSecondAddress(String secondAddress)
+	{
+		this.secondAddress = secondAddress;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Employee [id=" + id + ", name=" + name + ", firstAddress="
+				+ firstAddress + ", secondAddress=" + secondAddress + "]";
+	}
+
+}
