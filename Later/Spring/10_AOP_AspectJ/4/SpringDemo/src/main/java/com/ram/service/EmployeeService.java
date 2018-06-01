919cab9cac6d5e422e6b48944e35cdb0e01b2332
Ram <ramram_43210@yahoo.com>
2018-06-01 19:07:43 +0530

    1

---
+++ Later/Spring/10_AOP_AspectJ/4/SpringDemo/src/main/java/com/ram/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.ram.service;
+
+public class EmployeeService
+{
+	public String addEmployee()
+	{
+		System.out.println("Add Employee ");
+		return "Employee Peter information is added successfully";
+	}
+
+	public void modifyEmployee()
+	{
+		System.out.println("Modify Employee");
+	}
+
+	public void deleteEmployee()
+	{
+		System.out.println("Delete Employee");
+	}
+
+}
