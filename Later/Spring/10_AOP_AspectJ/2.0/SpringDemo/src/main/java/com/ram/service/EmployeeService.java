521ce6e6d4f70109146561e1712634602cf1c75d
Ram <ramram_43210@yahoo.com>
2018-06-21 13:37:46 +0530

    21

---
+++ Later/Spring/10_AOP_AspectJ/2.0/SpringDemo/src/main/java/com/ram/service/EmployeeService.java
@@ -0,0 +1,26 @@
+package com.ram.service;
+
+public class EmployeeService
+{
+	public void addEmployee()
+	{
+		System.out.println("addEmployee() method is called \n");
+	}
+
+	public void addEmployee(String name, int age)
+	{
+		System.out.println(
+				"addEmployee(String name, int age) method is called \n");
+	}
+
+	public void modifyEmployee()
+	{
+		System.out.println("modifyEmployee() method is called \n ");
+	}
+
+	public void deleteEmployee()
+	{
+		System.out.println("deleteEmployee() method is called \n ");
+	}
+
+}
