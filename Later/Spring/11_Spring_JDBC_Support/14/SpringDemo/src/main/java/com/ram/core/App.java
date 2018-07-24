1e87b82e5d1ced121e349350d8b3ed502b0436b6
Ram <ramram_43210@yahoo.com>
2018-07-24 09:29:40 +0530

    1

---
+++ Later/Spring/11_Spring_JDBC_Support/14/SpringDemo/src/main/java/com/ram/core/App.java
@@ -0,0 +1,32 @@
+package com.ram.core;
+
+import java.util.ArrayList;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ram.employee.dao.EmploeyeeDAO;
+import com.ram.employee.model.Employee;
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		Employee employee1 = new Employee(1, "Peter", 28, 80000);
+		Employee employee2 = new Employee(2, "Dave", 38, 90000);
+		Employee employee3 = new Employee(3, "John", 48, 10000);
+		ArrayList<Employee> employeeList = new ArrayList<Employee>();
+		employeeList.add(employee1);
+		employeeList.add(employee2);
+		employeeList.add(employee3);
+
+		EmploeyeeDAO employeeDAO = (EmploeyeeDAO) context
+				.getBean("employeeDAO");
+		employeeDAO.batchInsert(employeeList);
+		System.out.println(
+				"Employee records are inserted successfully.");
+	}
+}
