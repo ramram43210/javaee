45d08acf4695c1ccd2a58318bb6999e67607605f
Ram <ramram_43210@yahoo.com>
2018-08-14 20:54:10 +0530

    1

---
+++ Later/Spring/11_Spring_JDBC_Support/17/SpringDemo/src/main/java/com/ram/core/App.java
@@ -0,0 +1,22 @@
+package com.ram.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ram.employee.dao.EmploeyeeDAO;
+import com.ram.employee.model.Employee;
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		EmploeyeeDAO employeeDAO = (EmploeyeeDAO) context
+				.getBean("employeeDAO");
+		Employee employee1 = new Employee("Peter", 28, 80000);
+		employeeDAO.insert(employee1);
+		System.out.println("Employee record inserted successfully.");
+	}
+}
