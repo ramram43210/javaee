080780dd697d268ac361bcf097f2703dc443771a
Ram <ramram_43210@yahoo.com>
2018-10-30 10:42:56 +0530

    1

---
+++ Spring_2018/SpringDemo_BeanNameAutoProxyCreator/SpringDemo/src/main/java/com/ram/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.ram.service;
+
+public class EmployeeService
+{
+	public String getEmployeeName()
+	{
+		System.out.println("Get Employee Name");
+		return "Dave";
+	}
+	
+	public int getEmployeeAge()
+	{
+		System.out.println("Get Employee Age");
+		return 28;
+	}
+	
+	public String getEmployeeAddress()
+	{
+		System.out.println("Get Employee Address");
+		return "A111, East Street";
+	}
+}
