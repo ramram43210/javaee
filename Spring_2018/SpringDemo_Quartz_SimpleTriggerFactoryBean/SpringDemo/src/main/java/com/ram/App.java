b4b23860136a4e0b0dbc9c5c38d5b57ede949322
Ram <ramram_43210@yahoo.com>
2018-12-10 10:04:18 +0530

    1

---
+++ Spring_2018/SpringDemo_Quartz_SimpleTriggerFactoryBean/SpringDemo/src/main/java/com/ram/App.java
@@ -0,0 +1,14 @@
+package com.ram;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+	public static void main(String args[])
+	{
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"quartz-context.xml");
+	}
+
+}
